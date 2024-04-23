4dbb062e7109f9a591e6d3f3a76bd2961b2bb1c3
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-04-24 00:06:57 +0200

    Made the counter to be more dynamic

--- script.js
+++ script.js
@@ -13,6 +13,46 @@ window.addEventListener('scroll', function() {
       h1.classList.add('animate');
     }
   });
+  function animateValue(id, start, end, duration) {
+    let obj = document.getElementById(id);
+    let range = end - start;
+    let current = start;
+    let increment = range / (duration / 10); 
+    let timer = setInterval(function() {
+      current += increment;
+      obj.textContent = Math.round(current); 
+      if ((increment > 0 && current >= end) || (increment < 0 && current <= end)) {
+        clearInterval(timer);
+      }
+    }, 10); 
+  }
+  
+  function startCounterOnScroll() {
+    const counters = document.querySelectorAll('.counter-item');
+    counters.forEach(counter => {
+      if (isElementInViewport(counter)) {
+        const id = counter.querySelector('.count').id;
+        switch (id) {
+          case 'counter-downloads':
+            animateValue(id, 0, 200, 2000);
+            break;
+          case 'counter-staff':
+            animateValue(id, 0, 20, 1000);
+            break;
+          case 'counter-questions':
+            animateValue(id, 0, 4000, 2000);
+            break;
+          case 'counter-partners':
+            animateValue(id, 0, 10, 2000);
+            break;
+        }
+      }
+    });
+  }
+  
+  window.addEventListener('scroll', startCounterOnScroll);
+  
+  
 
 
 
